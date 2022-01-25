ff18c9b95fca549d270c7d0c79b762130a83b6a4
Sevrinn <sevrinnsevrinn@gmail.com>
2022-01-24 16:52:50 -0800

    added state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
 import './App.css'
 
+//set url to the api endpoint
+const url = 'https://course-api.com/react-tours-project'
+
 function App() {
+	//STATE
+	const [loading, setLoading] = useState(true)
+	const [tours, setTours] = useState([])
 	return (
 		<div className='App'>
 			<h1>lets gooooooo</h1>
